9efc787077e0c40a45e46ceb990406e570155dd9
Geoff Maciolek <geoffmaciolek@gmail.com>
2021-04-14 15:04:11 -0400

    Basic SQL queries

--- main.py
+++ main.py
@@ -19,11 +19,18 @@ debug_restore_file_count = 15 # Set to 0 or Null if not debugging!
 
 database_filename = 'manifest.db' # SQLite DB filename
 
+#example_query = '''SELECT "_rowid_",* FROM "main"."Files" WHERE "domain" LIKE '%CameraRollDomain%' LIMIT 0, 10;'''
+#example_query = '''SELECT "_rowid_",* FROM "main"."Files" WHERE "domain" LIKE '%CameraRollDomain%' AND "relativePath" LIKE '%Media/DCIM/100APPLE/%' LIMIT 0, 15;'''
+example_query = '''SELECT "_rowid_",fileID,domain,relativePath FROM "main"."Files" WHERE "domain" LIKE '%CameraRollDomain%' AND "relativePath" LIKE '%Media/DCIM/100APPLE/%' LIMIT 0, 15;'''
+
+base_query = '''SELECT "_rowid_",fileID,domain,relativePath FROM "main"."Files" WHERE "domain" LIKE '%?%' AND "relativePath" LIKE '%?%' LIMIT 0, 15;'''
+
 ### Script Start
 
 # Imports (and related functions as needed
 from pathlib import Path # So we can use sane paths
 import os, sqlite3
+
 if restore_timestamps_via_exif:
     from PIL import Image
     def get_date_taken(path): # A quick function to get the date
@@ -35,6 +42,17 @@ db_path = Path(backup_base_path,database_filename)
 file_info = os.stat(db_path)
 print(f'DB Size: {file_info.st_size/1024/1024:.1f}MB')
 
+# 'Connect' to SQLite DB
+con = sqlite3.connect(db_path)
 
+cur = con.cursor()
+#cur.execute(example_query)
+cur.execute(example_query,archive_domain,archive_path_match)
 
+one_row = cur.fetchone()
+print("One Row, 'raw' below")
+print(one_row)
+#print("\nOne row, unicode decoded maybe?" + str(one_row[0].decode('utf-8')))
+
+print("lol debug")
 # SELECT "_rowid_",* FROM "main"."Files" WHERE "domain" LIKE '%CameraRollDomain%' ESCAPE '\' LIMIT 0, 49999;
